9cf75919383f0dc497154a7c5c1a9bedb56bdd5c
Melissa Garza <melissa.garza@oracle.com>
2019-02-03 00:54:13 -0600

    Pass dummy data from router, through the template, do the front-end fitbod javascript file

--- public/javascripts/fitbod.js
+++ public/javascripts/fitbod.js
@@ -1,3 +1,7 @@
 var parseDate = d3.timeParse('%Y-%m-%d');
 
 console.log(parseDate);
+
+var fitbod = function(data) {
+  console.log(data);
+};
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Fitbod Charts' });
+  var data = processData();
+  res.render('index', {
+    title: 'Fitbod Charts',
+    data: JSON.stringify(data)
+  });
 });
+
+function processData() {
+  return {test: 'test'};
+}
 
 module.exports = router;
