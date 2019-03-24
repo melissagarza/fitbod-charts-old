3beeedd51937948fa4f36c08ad185ac8ad7728db
Melissa Garza <melissa.garza@oracle.com>
2019-03-24 00:29:25 -0500

    Await processing of CSV data

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ let path = require('path');
 let fs = require('fs');
 let csvParse = require('csv-parse');
 
-router.get('/', function(req, res) {
-  let data = processData();
+router.get('/', async function(req, res) {
+  let data = await processData();
   res.render('index', {
     title: 'Fitbod Charts',
     data: JSON.stringify(data)
@@ -13,37 +13,35 @@ router.get('/', function(req, res) {
 });
 
 function processData() {
-  let fitbodData = [];
-  let csvParser = csvParse({
-    delimiter: ',',
-    columns: function headersToLowerCase(headers) {
-      let lowercaseHeaders = headers.map(header => {
-        return header.toLowerCase();
-      });
-      console.log(lowercaseHeaders);
-      return lowercaseHeaders;
-    }
+  return new Promise((resolve, reject) => {
+    let fitbodData = [];
+    let csvParser = csvParse({
+      delimiter: ',',
+      columns: function headersToLowerCase(headers) {
+        let lowercaseHeaders = headers.map(header => {
+          return header.toLowerCase();
+        });
+        return lowercaseHeaders;
+      }
+    });
+
+    csvParser.on('readable', () => {
+      let record
+      while (record = csvParser.read()) {
+        fitbodData.push(record)
+      }
+    })
+
+    csvParser.on('error', error => {
+      reject(error.message)
+    })
+
+    csvParser.on('end', function() {
+      return resolve(fitbodData);
+    })
+
+    fs.createReadStream(path.join(__dirname, '../public/data/fitbod_workout.csv')).pipe(csvParser);
   });
-
-  csvParser.on('readable', () => {
-    let record
-    while (record = csvParser.read()) {
-      fitbodData.push(record)
-    }
-  })
-
-  csvParser.on('error', error => {
-    console.error(err.message)
-  })
-
-  csvParser.on('end', function() {
-    console.log('end');
-    console.log(fitbodData);
-  })
-
-  fs.createReadStream(path.join(__dirname, '../public/data/fitbod_workout.csv')).pipe(csvParser);
-
-  return fitbodData;
 }
 
 module.exports = router;
